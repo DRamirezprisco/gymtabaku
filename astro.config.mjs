4e8e71fe6bf3dadac01e9d880e59769a65fd3a3c
Daniela_Ramirez <ramirezpriscodanielalisbeth@gmail.com>
2026-06-04 21:50:27 -0500

    add site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import cloudflare from '@astrojs/cloudflare';
+import cloudflare from "@astrojs/cloudflare";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://gymtabaku.guillermoveraghanain.workers.dev",
   adapter: cloudflare(),
 
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
