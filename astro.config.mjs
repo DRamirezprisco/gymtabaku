069fdc43e5fa21c903ba41405210da2ae9ec5ab2
Daniela_Ramirez <ramirezpriscodanielalisbeth@gmail.com>
2026-08-24 18:17:59 -0500

    update astro 7.2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,20 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
-import cloudflare from "@astrojs/cloudflare";
+import cloudflare from '@astrojs/cloudflare';
 
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://gymtabaku.guillermoveraghanain.workers.dev",
-  output: "server",
-  adapter: cloudflare({
-    imageService: "passthrough",
-  }),
+  adapter: cloudflare(),
 
   vite: {
     plugins: [tailwindcss()],
-  },
+    server: {
+      watch: {
+        ignored: ['**/.astro/**', '**/.wrangler/**', '**/dist/**']
+      }
+    }
+  }
 });
